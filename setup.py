7ace5344ed65c50a480d4f7325ae5857f049fab3
Satya <samal.satya001@gmail.com>
2025-12-10 09:27:27 +0530

    requiremnts added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.1.0",
+    author="Satya",
+    author_email="samal.satya002@gmail.com",
+    packages=find_packages()
+)
